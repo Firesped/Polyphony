6335914a4e5c899f19cb732c561514539494c6af
Firesped <4176294+Firesped@users.noreply.github.com>
2022-08-03 12:51:09 -0700

    Update decode_token.py
    debug information for token fail

--- polyphony/helpers/decode_token.py
+++ polyphony/helpers/decode_token.py
@@ -1,9 +1,13 @@
 import base64
 import logging
 
+from polyphony.settings import DEBUG
+
 log = logging.getLogger(__name__)
 
 
 def decode_token(token: str):
     token = token.split(".")
+    if DEBUG is TRUE:
+        log.info(string(base64.b64decode(token[0])))
     return int(base64.b64decode(token[0]))
